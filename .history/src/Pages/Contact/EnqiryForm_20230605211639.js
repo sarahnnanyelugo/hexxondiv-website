52d6f7e447d85225e604695ae1d7adf4ca77f768
Sarah <sarahnnanyelugo@gmail.com>
2023-06-05 22:00:22 +0100

    form

--- .history/src/Pages/Contact/EnqiryForm_20230605211639.js
+++ .history/src/Pages/Contact/EnqiryForm_20230605211639.js
@@ -16,9 +16,9 @@ export const EnqiryForm = () => {
             <p>
               You're not going to hit a ridiculously long phone menu when you
               call us. Your email isn't going to the inbox abyss, never to be
-              seen or heard from again. At Hexxondiv{" "}
-              <span className="highlight">Tech Hub</span>, we provide the
-              exceptional service we'd want to experience ourselves!
+              seen or heard from again. At{" "}
+              <span className="highlight">Hexxondiv Tech Hub</span>, we provide
+              the exceptional service we'd want to experience ourselves!
             </p>
           </center>
         </div>
